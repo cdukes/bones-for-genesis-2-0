d19076e4ba81f3b8e7b964cb22e58565289f918f
eddiemachado <machado08@gmail.com>
2012-05-21 20:50:59 -0500

    added responsive query example

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -15,21 +15,92 @@ Modernizr.load([
     // If the browser doesn't support border-radius ()
     test : Modernizr.borderradius,
     // then load this script
-    nope : ['libs/selectivizr-min.js']
+    nope : []
 	}
 	
-	/* 
+	/*
 	for a list of supported Modernizr tests, view the docs:
 	http://www.modernizr.com/docs/#s2
 	for a full chart of what browsers support what, check out this link:
-	http://www.findmebyip.com/litmus/ 
+	http://www.findmebyip.com/litmus/
 	*/
 ]);
 
-// as the page loads, cal these scripts
-$(document).ready(function() {
-	
-	// put add all your scripts here (they will be loaded in the FOOTER)
+// as the page loads, call these scripts
+jQuery(document).ready(function($) {
 
+    /*
+    Responsive jQuery is a tricky thing.
+    There's a bunch of different ways to handle
+    it so, be sure to research and find the one
+    that works for you best.
+    */
+    
+    /* getting viewport width */
+    var responsive_viewport = $(window).width();
+    
+    /* if is below 481px */
+    if (responsive_viewport < 481) {
+    
+    } /* end smallest screen */
+    
+    /* if is larger than 481px */
+    if (responsive_viewport > 481) {
+        
+    } /* end larger than 481px */
+    
+    /* if is above 768px */
+    if (responsive_viewport > 768) {
+    
+        /* load gravatars */
+        $('.comment img[data-gravatar]').each(function(){
+            $(this).attr('src',$(this).attr('data-gravatar'));
+        });
+        
+    }
+    
+    /* off the bat large screen actions */
+    if (responsive_viewport > 1030) {
+        
+    }
+    
+	
+	// add all your scripts here
+	
  
 }); /* end of as page load scripts */
+
+/*! A fix for the iOS orientation change zoom bug.
+ Script by @scottjehl, rebound by @wilto.
+ MIT License.
+*/
+(function(w){
+	// This fix addresses an iOS bug, so return early if the UA claims it's something else.
+	if( !( /iPhone|iPad|iPod/.test( navigator.platform ) && navigator.userAgent.indexOf( "AppleWebKit" ) > -1 ) ){ return; }
+    var doc = w.document;
+    if( !doc.querySelector ){ return; }
+    var meta = doc.querySelector( "meta[name=viewport]" ),
+        initialContent = meta && meta.getAttribute( "content" ),
+        disabledZoom = initialContent + ",maximum-scale=1",
+        enabledZoom = initialContent + ",maximum-scale=10",
+        enabled = true,
+		x, y, z, aig;
+    if( !meta ){ return; }
+    function restoreZoom(){
+        meta.setAttribute( "content", enabledZoom );
+        enabled = true; }
+    function disableZoom(){
+        meta.setAttribute( "content", disabledZoom );
+        enabled = false; }
+    function checkTilt( e ){
+		aig = e.accelerationIncludingGravity;
+		x = Math.abs( aig.x );
+		y = Math.abs( aig.y );
+		z = Math.abs( aig.z );
+		// If portrait orientation and in one of the danger zones
+        if( !w.orientation && ( x > 7 || ( ( z > 6 && y < 8 || z < 8 && y > 6 ) && x > 5 ) ) ){
+			if( enabled ){ disableZoom(); } }
+		else if( !enabled ){ restoreZoom(); } }
+	w.addEventListener( "orientationchange", restoreZoom, false );
+	w.addEventListener( "devicemotion", checkTilt, false );
+})( this );
