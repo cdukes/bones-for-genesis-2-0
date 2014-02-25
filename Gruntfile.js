0a6c4709ee323c6ec8c16652542b1120167abb5f
Cooper Dukes <cooper@Coopers-MacBook-Pro.local>
2014-02-25 15:32:14 +0800

    grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,116 @@
+// See: http://24ways.org/2013/grunt-is-not-weird-and-hard/
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		clean: {
+			build: {
+				src: [ 'build' ]
+			},
+		},
+
+		concat: {
+			build: {
+				src: [
+					'bower_components/iOS-Orientationchange-Fix/ios-orientationchange-fix.js',
+					'bower_components/jquery.fitvids/jquery.fitvids.js',
+					'bower_components/jquery-placeholder/jquery.placeholder.js',
+					'bower_components/svgeezy/svgeezy.js',
+					'bower_components/superfish/dist/js/superfish.js',
+					'js/*.js'
+				],
+				dest: 'build/js/scripts.js',
+			}
+		},
+
+		uglify: {
+			options: {
+				preserveComments: 'some',
+			},
+			build: {
+				src: 'build/js/scripts.js',
+				dest: 'build/js/scripts.min.js'
+			}
+		},
+
+		sass: {
+			build: {
+				options: {
+					style: 'expanded'
+				},
+				files: {
+					'build/css/style.css': 'sass/style.scss'
+				}
+			}
+		},
+
+		autoprefixer: {
+			build: {
+				options: {
+					browsers: ['last 2 version', '> 1%', 'ie 8', 'ie 9'],
+					cascade: true,
+				},
+				src: 'build/css/style.css',
+				dest: 'build/css/style.prefixed.css',
+			},
+		},
+
+		cssmin: {
+			build: {
+				src: 'build/css/style.prefixed.css',
+				dest: 'build/css/style.min.css'
+			}
+		},
+
+		imagemin: {
+			build: {
+				files: [{
+					expand: true,
+					cwd: 'images/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'build/images/'
+				}]
+			}
+		},
+
+		watch: {
+			js: {
+				files: ['js/*.js'],
+				tasks: ['concat', 'uglify'],
+				options: {
+					spawn: false,
+				},
+			},
+
+			css: {
+				files: ['sass/*.scss'],
+				tasks: ['sass', 'autoprefixer', 'cssmin'],
+				options: {
+					spawn: false,
+				}
+			},
+
+			images: {
+				files: ['images/*'],
+				tasks: ['imagemin'],
+				options: {
+					spawn: false,
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['clean', 'concat', 'uglify', 'imagemin', 'sass', 'autoprefixer', 'cssmin', 'watch']);
+
+};
